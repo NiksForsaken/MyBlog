2e2ce28f80dca162fdd0c5cc31684774254b9cc9
NiksForsaken <65817175+NiksForsaken@users.noreply.github.com>
2021-03-30 11:19:44 +0500

    Update settings.py

--- SixtyFPS/settings.py
+++ SixtyFPS/settings.py
@@ -19,11 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xg#wx*)-iu*y-=hf66)5o%3^s#%$1jqxcp&uv%kpaavk%(jp@r'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
